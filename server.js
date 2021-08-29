ff2913fafdd63ab9391b8acc14626c80359a96e1
Maxx <maxhiga.hawaii@gmail.com>
2021-08-28 23:43:33 -1000

    added dependencies

---
+++ server.js
@@ -0,0 +1,6 @@
+//requiring all dependencies
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+const path = require("path");
