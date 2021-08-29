6e09e4f0880d5236da60b31d5996b492c7a3ee1e
Maxx <maxhiga.hawaii@gmail.com>
2021-08-28 23:47:24 -1000

    added gitignore because not saved earlier, added rest of server file

--- server.js
+++ server.js
@@ -4,3 +4,31 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//mongoose connection
+mongoose.connect(
+  process.envMONGODB_URI || "mongodb://localhost/authenticationLogIn",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("connected to mongodb");
+  }
+);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
