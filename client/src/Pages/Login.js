b69614258b5e0e2031dab3ac4de8b67ae6fb10f9
Maxx <maxhiga.hawaii@gmail.com>
2021-10-03 23:08:52 -1000

    starting page

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -4,6 +4,14 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import "../App.css";
 
-const Login = () => {};
+const Login = () => {
+  const [form, setForm] = useState();
+
+  return <div className="page">
+      <form onSubmit={}>
+          <h1>Login</h1>
+      </form>
+  </div>;
+};
 
 export default Login;
