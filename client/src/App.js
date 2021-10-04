0608fa56c12b7976844d0c9087e15f0ce8e94d5d
Maxx <maxhiga.hawaii@gmail.com>
2021-10-03 23:11:36 -1000

    added route and started route path for login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import UserContext from "./Context/UserContext";
+
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Router>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Route exact path="/" component={Login} />
+        </UserContext.Provider>
+      </Router>
     </div>
   );
 }
