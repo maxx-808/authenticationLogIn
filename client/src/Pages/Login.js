cc9f624342fbc6d9e162d5191ff371b4ec99c1fc
Maxx <maxhiga.hawaii@gmail.com>
2021-10-03 23:06:50 -1000

    changed landing.js to login.js added imports

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,9 @@
+import React, { useState, useContext, useEffect } from "react";
+import UserContext from "../Context/UserContext.js";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import "../App.css";
+
+const Login = () => {};
+
+export default Login;
